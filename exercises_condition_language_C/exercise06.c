2b82945548ac64d28853ecb695681eeb0cdf017e
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-29 19:16:10 -0300

    exercise 06

---
+++ exercises_condition_language_C/exercise06.c
@@ -0,0 +1,44 @@
+/*
+Author: Francisco Gomes da Silva Junior
+College: Estacio de Sa
+Period: 1
+Course: Computer Science
+Discipline: Introduction Programming a language C
+Description: Read a three notes and calculate a average of notes and printing final situation. If approved, reproved or in recuperation.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // Section of variables
+    float note_one, note_two, note_three, average_notes;
+
+    // Read data user input
+    printf("Note one: \n");
+    scanf("%f", &note_one);
+
+    printf("Note two: \n");
+    scanf("%f", &note_two);
+
+    printf("Note three: \n");
+    scanf("%f", &note_three);
+
+    average_notes = (note_one + note_two + note_three) / 3.0;
+
+    // Validation of situation student
+    if (average_notes >= 7)
+    {
+        printf("Congrats. You is approved. You average notes is: %.2f", average_notes);
+    }
+    else if (average_notes < 7 && average_notes >= 5)
+    {
+        printf("In recuperation. You average of notes: %.2f", average_notes);
+    }
+    else
+    {
+        printf("You is reproved. Try again. You average of notes: %.2f", average_notes);
+    }
+
+    return 0;
+}
